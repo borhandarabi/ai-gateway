34576b9f63e7efa2194acc4317e6885b1f2da59b
borhan <borhan.d@gmail.com>
2026-08-18 22:35:19 +0330

    fix: QWEN2API_REPO url

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ ZAI_REPO="${ZAI_REPO:-https://github.com/borhandarabi/GLM-Free-API.git#main}"
 KIMI_REPO="${KIMI_REPO:-https://github.com/izaart95-jpg/KimiFreeAPI.git#main}"
 DEEPSEEK_REPO="${DEEPSEEK_REPO:-https://github.com/izaart95-jpg/DeepSeekFreeAPI.git#main}"
 FLARESOLVERR_REPO="${FLARESOLVERR_REPO:-https://github.com/Rorqualx/flaresolverr-go.git#main}"
+QWEN2API_REPO="${QWEN2API_REPO:-https://github.com/Rorqualx/flaresolverr-go.git#main}"
 # OmniRoute is pulled as a prebuilt base image, not built from source 
 OMNIROUTE_IMAGE="${OMNIROUTE_IMAGE:-diegosouzapw/omniroute:3.8.49-web}"
 TAG="${TAG:-ai-gateway:local}"
@@ -20,6 +21,7 @@ docker buildx build \
   --build-context kimi_src="${KIMI_REPO}" \
   --build-context deepseek_src="${DEEPSEEK_REPO}" \
   --build-context flaresolverr_src="${FLARESOLVERR_REPO}" \
+  --build-context qwen2api_src="${QWEN2API_REPO}" \
   --build-arg OMNIROUTE_IMAGE="${OMNIROUTE_IMAGE}" \
   -t "${TAG}" \
   --load \
