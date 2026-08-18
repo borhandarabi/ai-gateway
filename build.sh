b4d6cbc79368de0754a1b1d5d17071e375fd3859
borhan <borhan.d@gmail.com>
2026-08-18 22:19:12 +0330

    fix: FLARESOLVERR_REPO url

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ GROK2API_REPO="${GROK2API_REPO:-https://github.com/chenyme/grok2api.git#main}"
 ZAI_REPO="${ZAI_REPO:-https://github.com/borhandarabi/GLM-Free-API.git#main}"
 KIMI_REPO="${KIMI_REPO:-https://github.com/izaart95-jpg/KimiFreeAPI.git#main}"
 DEEPSEEK_REPO="${DEEPSEEK_REPO:-https://github.com/izaart95-jpg/DeepSeekFreeAPI.git#main}"
+FLARESOLVERR_REPO="${FLARESOLVERR_REPO:-https://github.com/Rorqualx/flaresolverr-go.git#main}"
 # OmniRoute is pulled as a prebuilt base image, not built from source 
 OMNIROUTE_IMAGE="${OMNIROUTE_IMAGE:-diegosouzapw/omniroute:3.8.49-web}"
 TAG="${TAG:-ai-gateway:local}"
@@ -18,6 +19,7 @@ docker buildx build \
   --build-context zai_src="${ZAI_REPO}" \
   --build-context kimi_src="${KIMI_REPO}" \
   --build-context deepseek_src="${DEEPSEEK_REPO}" \
+  --build-context flaresolverr_src="${FLARESOLVERR_REPO}" \
   --build-arg OMNIROUTE_IMAGE="${OMNIROUTE_IMAGE}" \
   -t "${TAG}" \
   --load \
